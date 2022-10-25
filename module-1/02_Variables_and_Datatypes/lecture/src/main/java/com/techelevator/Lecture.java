c6a55e3c5420ea45c86f14f6ef046fff3eeaef53
Alexis Horst <alexisl.horst@gmail.com>
2022-10-25 12:56:45 -0400

    variables-and-datatypes lecture code complete

--- module-1/02_Variables_and_Datatypes/lecture/src/main/java/com/techelevator/Lecture.java
+++ module-1/02_Variables_and_Datatypes/lecture/src/main/java/com/techelevator/Lecture.java
@@ -8,124 +8,145 @@ public class Lecture {
 		1. Create a variable to hold an int and call it numberOfExercises.
 			Then set it to 26.
 		*/
+		int numberOfExercises = 26;
 
-		//System.out.println(numberOfExercises);
+		System.out.println(numberOfExercises);
 
 		/*
 		2. Create a variable to hold a double and call it half.
 			Set it to 0.5.
 		*/
-
-		//System.out.println(half);
+		double half = 0.5;
+		System.out.println(half);
 
 		/*
 		3. Create a variable to hold a String and call it name.
 			Set it to "TechElevator".
 		*/
-
-		//System.out.println(name);
+		String name = "TechElevator";
+		System.out.println(name);
 
 		/*
 		4. Create a variable called seasonsOfFirefly and set it to 1.
 		*/
-
-		//System.out.println(seasonsOfFirefly);
+		int seasonsOfFirefly = 1;
+		System.out.println(seasonsOfFirefly);
 
 		/*
 		5. Create a variable called myFavoriteLanguage and set it to "Java".
 		*/
-
-		//System.out.println(myFavoriteLanguage);
+		String myFavoriteLanguage = "Java";
+		System.out.println(myFavoriteLanguage);
 
 		/*
 		6. Create a variable called pi and set it to 3.1416.
 		*/
-
-		//System.out.println(pi);
+		double pi = 3.1416;
+		System.out.println(pi);
 
 		/*
 		7. Create and set a variable that holds your name.
 		*/
-
+		String myName = "Lexi";
+		System.out.println(myName);
 		/*
 		8. Create and set a variable that holds the number of buttons on your mouse.
 		*/
-
+		int myMouseButtons = 4;
+		System.out.println(myMouseButtons);
 		/*
 		9. Create and set a variable that holds the percentage of battery left on
 		your phone.
 		*/
-
+		int percentagePhoneBatteryLeft = 90;
+		System.out.println(percentagePhoneBatteryLeft);
 		/*
 		10. Create an int variable that holds the difference between 121 and 27.
 		*/
-
+		int x = 121 / 27;
+		System.out.println(x);
 		/*
 		11. Create a double that holds the addition of 12.3 and 32.1.
 		*/
-
+		double y = 12.3 + 32.1;
+		System.out.println(y);
 		/*
 		12. Create a String that holds your full name.
 		*/
-
+		String myFullName = "Alexis Horst";
+		System.out.println(myFullName);
 		/*
 		13. Create a String that holds the word "Hello, " concatenated onto your
 		name from above.
 		*/
-
+		String greetingName = "Hello " + myFullName + "!";
+		System.out.println(greetingName);
 		/*
 		14. Add a " Esquire" onto the end of your full name and save it back to
 		the same variable.
 		*/
-
+		myFullName = myFullName + " Esquire";
+		System.out.println(myFullName);
 		/*
 		15. Now do the same as exercise 14, but use the += operator.
 		*/
-
+		myFullName += " Esquire";
+		System.out.println(myFullName);
 		/*
 		16. Create a variable to hold "Saw" and add a 2 onto the end of it.
 		*/
-
+		String horrorMovie = "Saw";
+		horrorMovie += 2;
+		System.out.println(horrorMovie);
 		/*
 		17. Add a 0 onto the end of the variable from exercise 16.
 		*/
-
+		horrorMovie += 0;
+		System.out.println(horrorMovie);
 		/*
 		18. What is 4.4 divided by 2.2?
 		*/
-
+		double divides = 4.4 / 2.2;
+		System.out.println(divides);
 		/*
 		19. What is 5.4 divided by 2?
 		*/
-
+		double z = 5.4 / 2;
+		System.out.println(z);
 		/*
 		20. What is 5 divided by 2?
 		*/
-
+		int moreDivides = 5 / 2;
+		System.out.println(moreDivides);
 		/*
 		21. What is 5.0 divided by 2?
 		*/
-
+		double evenMoreDivision = 5.0 / 2;
+		System.out.println(evenMoreDivision);
 		/*
 		22. What is 66.6 divided by 100? Is the answer you get right or wrong?
 		*/
-
+		double lastDivision = 66.6 / 100;
+		System.out.println(lastDivision);
 		/*
 		23. If I divide 5 by 2, what's my remainder?
 		*/
-
+		int whatsMyRemainder = 5 % 2;
+		System.out.println(whatsMyRemainder);
 		/*
 		24. What is 1,000,000,000 * 3?
 		*/
-
+		int bigNumbers = 1000000000 * 3;
+		System.out.println(bigNumbers);
 		/*
 		25. Create a variable that holds a boolean called isDoneWithExercises and
 		set it to false.
 		*/
-
+		Boolean isDoneWithExercises = false;
 		/*
 		26. Now set isDoneWithExercise to true.
 		*/
+		Boolean isReallyDoneWithExercises = true;
 		
 	}
 
