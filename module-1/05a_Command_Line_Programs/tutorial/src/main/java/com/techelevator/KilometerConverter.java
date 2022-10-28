416bc1a98decc60a61ad4880073c45c3b6ef2f9d
Alexis Horst <alexisl.horst@gmail.com>
2022-10-28 09:07:57 -0400

    command-line-programs tutorial partial complete

--- module-1/05a_Command_Line_Programs/tutorial/src/main/java/com/techelevator/KilometerConverter.java
+++ module-1/05a_Command_Line_Programs/tutorial/src/main/java/com/techelevator/KilometerConverter.java
@@ -3,6 +3,24 @@ package com.techelevator;
 import java.util.Scanner;
 
 public class KilometerConverter {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please kilometer value to start at: ");
+        String value = input.nextLine();
+        int kilometerStart = Integer.parseInt(value);
+
+        System.out.println("Please kilometer value to end with: ");
+        value = input.nextLine();
+        int kilometerEnd = Integer.parseInt(value);
+
+        System.out.println("Please kilometer value to increment by: ");
+        value = input.nextLine();
+        int kilometerIncrement = Integer.parseInt(value);
+
+        System.out.println("Going from " + kilometerStart + "km to " + kilometerEnd +
+                "km in increments of " + kilometerIncrement + "km.");
+    }
 
 
     
