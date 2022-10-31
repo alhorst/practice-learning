7f97ee31e4e29c59ae8e916f57c48700158275dd
Alexis Horst <alexisl.horst@gmail.com>
2022-10-31 12:07:54 -0400

    objects-and-strings lecture code complete

--- module-1/06_Intro_to_Objects_Strings/lecture/src/main/java/com/techelevator/Lecture.java
+++ module-1/06_Intro_to_Objects_Strings/lecture/src/main/java/com/techelevator/Lecture.java
@@ -1,26 +1,62 @@
 package com.techelevator;
 
+import java.util.Arrays;
+
 public class Lecture {
 
 	public static void main(String[] args) {
+
+		int [] myArray = new int[10]; //declare an array integer
+		System.out.println(myArray); //prints a hexadecimal address
+
+		for (int i = 0; i < myArray.length; i++) {
+			System.out.println(myArray[i]); //all values initialized to zero
+		}
+
+		Arrays.fill(myArray, 56); //replace all values in array with 56
+
+		System.out.println(Arrays.toString(myArray)); //converts the array of ints to a string representation
+		System.out.println("Replaced all values with 56");
+
+		int lengthOfNewArray = 5;
+		int[] newArray = Arrays.copyOf(myArray, lengthOfNewArray); //take first five values of old array and put in new array
+		System.out.println(Arrays.toString(newArray)); //now we can see new array has a length of five with values copied from old array
+
+		newArray[1] = 3; //reassigning values stored in newArray
+		newArray[2] = 13;
+		newArray[3] = 91;
+		newArray[4] = 5; //will now print [56, 3, 13, 91, 5]
+
+		System.out.println("After reassigning, values are: " + Arrays.toString(newArray));
+
+		Arrays.sort(newArray); //this is an in-memory sort, do not have to reassign
+		System.out.println("The sorted array is: " + Arrays.toString(newArray));
 
 		System.out.println("************************************");
 		System.out.println("****** MAKING A STRING OBJECT ******");
 		System.out.println("************************************");
 
-		/* The String class gets special treatment in the Java language.  One
-		 * example of this is that there is a literal representation of a
-		 * String (i.e. characters appearing between two double quotes.  This
-		 * is not the case for most classes */
-
 		/* create an new instance of String using a literal */
-
+		String greeting = "Hello, hello, can anybody hear me?"; //strings do not need to be declared with 'new'
+		System.out.println(greeting);
 		
 		System.out.println();
 		System.out.println("******************************");
 		System.out.println("****** MEMBER METHODS ******");
 		System.out.println("******************************");
 		System.out.println();
+
+		System.out.println("Length of greeting is: " + greeting.length());
+		char first = greeting.charAt(0); //first character in position zero
+		char last = greeting.charAt(greeting.length() - 1);
+
+		int count = 0;
+		for (int i = 0; i < greeting.length(); i++) {
+			if (greeting.charAt(i) == 'o') {
+				count++;
+			}
+		}
+		System.out.println("There are " + count + " 'o's in our greeting.");
 
 
 		/* Other commonly used methods:
