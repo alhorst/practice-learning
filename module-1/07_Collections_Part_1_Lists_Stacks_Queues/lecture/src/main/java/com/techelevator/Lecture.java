71f2dad0fd3b6f2993c83c462f20ee283bb37862
Alexis Horst <alexisl.horst@gmail.com>
2022-11-01 12:00:40 -0400

    collections-part-1 lecture code complete

--- module-1/07_Collections_Part_1_Lists_Stacks_Queues/lecture/src/main/java/com/techelevator/Lecture.java
+++ module-1/07_Collections_Part_1_Lists_Stacks_Queues/lecture/src/main/java/com/techelevator/Lecture.java
@@ -1,6 +1,7 @@
 package com.techelevator;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -11,57 +12,87 @@ public class Lecture {
 		System.out.println("       LISTS");
 		System.out.println("####################");
 
+		List<String> names = new ArrayList<>();
 
 		System.out.println("####################");
 		System.out.println("Lists are ordered");
 		System.out.println("####################");
 
+		names.add("Bilbo");
+		names.add("Frodo");
+		names.add("Sam");
+		names.add("Merry");
+		names.add("Gandalf");
 
 		System.out.println("####################");
 		System.out.println("Lists allow duplicates");
 		System.out.println("####################");
+
+		names.add("Bilbo");
+		printList(names);
 
 
 		System.out.println("####################");
 		System.out.println("Lists allow elements to be inserted in the middle");
 		System.out.println("####################");
 
+		names.add(3, "Pippin");
 
 		System.out.println("####################");
 		System.out.println("Lists allow elements to be removed by index");
 		System.out.println("####################");
 
+		names.remove(0);
 
 		System.out.println("####################");
 		System.out.println("Find out if something is already in the List");
 		System.out.println("####################");
 
+		names.contains("Bilbo");
 
 		System.out.println("####################");
 		System.out.println("Find index of item in List");
 		System.out.println("####################");
 
+		System.out.println("Frodo is at index " + names.indexOf("Frodo"));
 
 		System.out.println("####################");
 		System.out.println("Lists can be turned into an array");
 		System.out.println("####################");
 
+		String[] namesArray2 = names.toArray(new String[0]); //what data type is passed in and what index
+		//can also use names.size() if you don't know List size
+		System.out.println(Arrays.toString(namesArray2));
 
 		System.out.println("####################");
 		System.out.println("Lists can be sorted");
 		System.out.println("####################");
+
+		Collections.sort(names); //in memory sort, can't go back
 
 
 		System.out.println("####################");
 		System.out.println("Lists can be reversed too");
 		System.out.println("####################");
 
+		Collections.reverse(names); //in memory reverse, can't go back
 
 		System.out.println("####################");
 		System.out.println("       FOREACH");
 		System.out.println("####################");
 		System.out.println();
 
+		for(String item : names ) {
+			System.out.println("Character: " + item);
+		}
 
+
+	}
+
+	public static void printList(List<String> names) {
+		for (int i = 0; i < names.size(); i++) {
+			System.out.println((i + 1) + ") " + names.get(i));
+
+		}
 	}
 }
