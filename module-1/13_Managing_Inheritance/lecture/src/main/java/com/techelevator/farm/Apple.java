f92fae758c3006bdfce63eb220877f0924b280f4
Margaret Green <margaret@techelevator.com>
2022-11-09 07:34:39 -0500

    added day 13 lecture and slides

---
+++ module-1/13_Managing_Inheritance/lecture/src/main/java/com/techelevator/farm/Apple.java
@@ -0,0 +1,14 @@
+package com.techelevator.farm;
+
+import java.math.BigDecimal;
+
+public class Apple implements Sellable{
+    @Override
+    public BigDecimal getPrice() {
+        //BigDecimal is how Java deals with MONEY!
+        // created a pricePerApple variable (object)
+        BigDecimal pricePerApple = new BigDecimal("0.49");
+        return pricePerApple;
+        // return (new BigDecimal("0.49"));
+    }
+}
