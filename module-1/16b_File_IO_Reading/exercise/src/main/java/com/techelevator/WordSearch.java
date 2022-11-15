2f4a7f490de2d38ec92572eb7abfe55d6ec19399
Alexis Horst <alexisl.horst@gmail.com>
2022-11-15 09:04:12 -0500

    file-io-reading-exceptions exercise partial complete

--- module-1/16b_File_IO_Reading/exercise/src/main/java/com/techelevator/WordSearch.java
+++ module-1/16b_File_IO_Reading/exercise/src/main/java/com/techelevator/WordSearch.java
@@ -1,8 +1,25 @@
 package com.techelevator;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class WordSearch {
 
 	public static void main(String[] args) {
+
+
+		//make this a method
+		System.out.println("Please enter a file path: ");
+		Scanner userInput = new Scanner(System.in);
+		String fileToFind = userInput.nextLine();
+		File inputFile = new File(fileToFind);
+
+		System.out.println("Please enter a word to find: ");
+		String wordToFind = userInput.nextLine();
+
+
+
+
 
 	}
 
