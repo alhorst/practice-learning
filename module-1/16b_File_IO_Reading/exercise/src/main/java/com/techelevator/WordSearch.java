72828a427ee91cf3bf52fa10abb8562c34b58776
Alexis Horst <alexisl.horst@gmail.com>
2022-11-15 19:14:11 -0500

    file-io-reading-exceptions exercise complete

--- module-1/16b_File_IO_Reading/exercise/src/main/java/com/techelevator/WordSearch.java
+++ module-1/16b_File_IO_Reading/exercise/src/main/java/com/techelevator/WordSearch.java
@@ -1,26 +1,39 @@
 package com.techelevator;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class WordSearch {
-
 	public static void main(String[] args) {
-
-
-		//make this a method
-		System.out.println("Please enter a file path: ");
 		Scanner userInput = new Scanner(System.in);
-		String fileToFind = userInput.nextLine();
-		File inputFile = new File(fileToFind);
-
-		System.out.println("Please enter a word to find: ");
+		System.out.println("Please enter a file path:");
+		String filePath = userInput.nextLine();
+		System.out.println("What word are you looking for?");
 		String wordToFind = userInput.nextLine();
+		System.out.println("Should the search be case sensitive? Y or N?");
+		String yesOrNo = userInput.nextLine();
+		File aliceFile = new File(filePath);
 
+		try {
+			userInput = new Scanner(aliceFile);
+			int lineNumber = 1;
+			while (userInput.hasNextLine()) {
+				String line = userInput.nextLine();
 
-
-
-
+				if (yesOrNo.equals("Y")) {
+					if (line.contains(wordToFind)) {
+						System.out.println(lineNumber + ") " + line);
+					}
+				} else if (yesOrNo.equals("N")) {
+					if (line.toLowerCase().contains(wordToFind.toLowerCase())){
+						System.out.println(lineNumber + ") " + line);
+					}
+				} lineNumber++;
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found or unable to be read from.");
+		}
 	}
-
 }
+
