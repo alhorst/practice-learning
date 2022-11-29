dd3156159313f930ddf14aeecd010b57a9e27510
Alexis Horst <alexisl.horst@gmail.com>
2022-11-28 19:16:17 -0500

    insert-update-and-delete exercise complete

--- module-2/04_INSERT_UPDATE_and_DELETE/exercise/Exercises/04_add_genre.sql
+++ module-2/04_INSERT_UPDATE_and_DELETE/exercise/Exercises/04_add_genre.sql
@@ -1,2 +1,8 @@
 -- 4. Add a "Sports" genre to the genre table. Add the movie "Coach Carter" to the newly created genre. (1 row each)
+BEGIN TRANSACTION;
+INSERT INTO genre (genre_name)
+VALUES ('Sports');
 
+INSERT INTO movie_genre (movie_id, genre_id)
+VALUES ('7214', (SELECT genre_id FROM genre WHERE genre_name = 'Sports'));
+COMMIT;
