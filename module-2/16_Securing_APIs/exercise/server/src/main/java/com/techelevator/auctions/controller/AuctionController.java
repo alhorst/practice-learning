905c55586b89498a5f3c1f7804620105ca504e50
Alexis Horst <alexisl.horst@gmail.com>
2022-12-13 20:12:32 -0500

    securing-apis exercise complete

--- module-2/16_Securing_APIs/exercise/server/src/main/java/com/techelevator/auctions/controller/AuctionController.java
+++ module-2/16_Securing_APIs/exercise/server/src/main/java/com/techelevator/auctions/controller/AuctionController.java
@@ -4,13 +4,16 @@ import com.techelevator.auctions.dao.AuctionDao;
 import com.techelevator.auctions.dao.MemoryAuctionDao;
 import com.techelevator.auctions.model.Auction;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
+import java.security.Principal;
 import java.util.List;
 
 @RestController
+@PreAuthorize("isAuthenticated()")
 @RequestMapping("/auctions")
 public class AuctionController {
 
@@ -20,6 +23,7 @@ public class AuctionController {
         this.dao = new MemoryAuctionDao();
     }
 
+    @PreAuthorize("permitAll")
     @RequestMapping(path = "", method = RequestMethod.GET)
     public List<Auction> list(@RequestParam(defaultValue = "") String title_like, @RequestParam(defaultValue = "0") double currentBid_lte) {
 
@@ -43,12 +47,14 @@ public class AuctionController {
         }
     }
 
+    @PreAuthorize("hasAnyRole('CREATOR','ADMIN')")
     @ResponseStatus(HttpStatus.CREATED)
     @RequestMapping(path = "", method = RequestMethod.POST)
     public Auction create(@Valid @RequestBody Auction auction) {
         return dao.create(auction);
     }
 
+    @PreAuthorize("hasAnyRole('CREATOR','ADMIN')")
     @RequestMapping(path = "/{id}", method = RequestMethod.PUT)
     public Auction update(@Valid @RequestBody Auction auction, @PathVariable int id) {
         Auction updatedAuction = dao.update(auction, id);
@@ -59,15 +65,17 @@ public class AuctionController {
         }
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @RequestMapping(path = "/{id}", method = RequestMethod.DELETE)
     public void delete(@PathVariable int id) {
         dao.delete(id);
     }
 
-    @RequestMapping(path = "/whoami")
-    public String whoAmI() {
-        return "";
+    @RequestMapping(path = "/whoami", method = RequestMethod.GET)
+    @ResponseBody
+    public String whoAmI(Principal principal) {
+        return principal.getName();
     }
 
 }
