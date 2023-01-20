e24bf39bd41cf1da9feae7e8696712951a818202
Alexis Horst <alexisl.horst@gmail.com>
2023-01-20 09:18:48 -0500

    vue-component-com lecture code complete

--- module-3/13_Vue_Component_Communication/lecture/src/components/ReviewDisplay.vue
+++ module-3/13_Vue_Component_Communication/lecture/src/components/ReviewDisplay.vue
@@ -2,22 +2,14 @@
   <div class="review" v-bind:class="{ favorited: review.favorited }" v-bind:key="review.id">
     <h4>{{ review.reviewer }}</h4>
     <div class="rating">
-      <img
-        src="../assets/star.png"
-        v-bind:title="review.rating + ' Star Review'"
-        class="ratingStar"
-        v-for="n in review.rating"
-        v-bind:key="n"
-      />
+      <img src="../assets/star.png" v-bind:title="review.rating + ' Star Review'" class="ratingStar"
+        v-for="n in review.rating" v-bind:key="n" />
     </div>
     <h3>{{ review.title }}</h3>
     <p>{{ review.review }}</p>
     <p>
       Favorite?
-      <input
-        type="checkbox"
-        v-bind:checked="review.favorited"
-      />
+      <input type="checkbox" v-bind:checked="review.favorited" v-on:change="onFavoritedChange()" />
     </p>
   </div>
 </template>
@@ -28,7 +20,7 @@ export default {
   props: ["review"],
   methods: {
     onFavoritedChange() {
-
+      this.$store.commit('FLIP_FAVORITED', this.review);
     }
   }
 };
