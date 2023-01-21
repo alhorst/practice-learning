0ee173e98e5323da4856bf5b6d4aa39e7c248666
Alexis Horst <alexisl.horst@gmail.com>
2023-01-21 13:58:59 -0500

    vue-component-comm exercise complete

---
+++ module-3/13_Vue_Component_Communication/exercise/src/components/NewBookForm.vue
@@ -0,0 +1,30 @@
+<template>
+  <form class="new-book-form" v-on:keyup.enter="add">
+    <input type="text" class="title-input" v-model="book.title" />
+    <input type="text" class="author-input" v-model="book.author" />
+    <input type="text" class="isbn-input" v-model="book.isbn" />
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      book: {
+        title: "",
+        author: "",
+        isbn: "",
+        read: false,
+      },
+    };
+  },
+  methods: {
+    add() {
+      this.$store.commit("ADD_NEW", this.book);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
