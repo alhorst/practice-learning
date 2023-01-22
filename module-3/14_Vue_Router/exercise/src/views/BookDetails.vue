3dd5f1589b03f85071051a586decb67c9c03d132
Alexis Horst <alexisl.horst@gmail.com>
2023-01-21 21:44:17 -0500

    vue-router exercise almost complete

---
+++ module-3/14_Vue_Router/exercise/src/views/BookDetails.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <book-card v-bind:book="book" v-bind:key="book.isbn" />
+  </div>
+</template>
+
+<script>
+import BookCard from "../components/BookCard.vue";
+export default {
+  components: { BookCard },
+  computed: {
+    book() {
+      return this.$store.state.books.find((book) => {
+        return book.isbn === this.$route.params.isbn;
+      });
+    },
+  },
+};
+</script>
+
+<style>
+</style>
