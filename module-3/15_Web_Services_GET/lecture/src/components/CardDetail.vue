56951783eb0982c599dc2c03f2ab64dcac0c5299
Alexis Horst <alexisl.horst@gmail.com>
2023-01-24 12:15:00 -0500

    webservices-get lecture code

--- module-3/15_Web_Services_GET/lecture/src/components/CardDetail.vue
+++ module-3/15_Web_Services_GET/lecture/src/components/CardDetail.vue
@@ -1,9 +1,36 @@
 <template>
-  <div></div>
+  <div>
+    <div class="loading"></div>
+    <h1>{{ card.title }}</h1>
+    <p>{{ card.description }}</p>
+    <comments-list v-bind:comments="card.comments" />
+  </div>
 </template>
 
 <script>
+import boardService from "../services/BoardService"
+import CommentsList from "./CommentsList.vue";
+
 export default {
-  name: "card-detail"
+  name: "card-detail",
+  components: { CommentsList },
+  data() {
+    return {
+      card: {
+        title: "",
+        description: "",
+        status: "",
+        comments: [],
+      },
+      isLoading: true,
+    };
+  },
+  created() {
+    this.card = boardService.getCard(this.$route.params.boardID, this.$route.params.cardID)
+      .then((response) => {
+        this.card = response;
+        this.isLoading = false;
+      })
+  }
 };
 </script>
--- module-3/15_Web_Services_GET/lecture/src/components/CommentsList.vue
+++ module-3/15_Web_Services_GET/lecture/src/components/CommentsList.vue
@@ -1,5 +1,11 @@
 <template>
-  <div></div>
+  <div class="comments">
+    <h2>Comments</h2>
+    <div class="comment" v-for="comment in comments" v-bind:key="comment.id"></div>
+    <h3>{{ comment.author }}</h3>
+    <p class="postedOn">{{ comment.postedOn }}</p>
+    <p>{{ comment.body }}</p>
+  </div>
 </template>
 
 <script>
@@ -14,19 +20,24 @@ export default {
   border-radius: 10px;
   padding: 10px 20px 20px 20px;
 }
+
 .comments h2 {
   margin: 0 0 30px 0;
 }
+
 .comment {
   margin: 20px 0;
   border-bottom: 1px solid rgb(180, 180, 180);
 }
+
 .comment:last-child {
   border-bottom: none;
 }
+
 .comment h3 {
   margin-bottom: 0;
 }
+
 .postedOn {
   margin-top: 4px;
   font-size: 0.75rem;
